3d10c7102c4b3c099c42bb36c95db464605fb854
gary_lin <gary_lin@cmoney.com.tw>
2026-08-07 00:41:19 +0800

    新增 SQL

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,9 +9,45 @@
       <li>
         <router-link to="/read/digitalLogic">數位邏輯</router-link>  
       </li>
+    </ul>
+    <ul>
       <li>
-        <router-link to="/read/sqlTransaction">資料庫-交易</router-link>  
+        <router-link to="/read/sql_ANSI">4-1.ANSI SPARC 及資料庫種類</router-link>  
       </li>
+      <li>
+        <router-link to="/read/sql_basic">4-2. 基礎及阿姆斯狀公理</router-link>  
+      </li>
+      <li>
+        <router-link to="/read/sql_designProcess">4-3. 資料庫設計流程</router-link>  
+      </li>
+      <li>
+        <router-link to="/read/sql_ObjectType">4-4. 資料庫物件種類</router-link>  
+      </li>
+      <li>
+        <router-link to="/read/sql_Intro">4-5. SQL種類</router-link>  
+      </li>
+      
+      <!-- <li>
+        <router-link to="/read/sql_Create">4-6. 建表</router-link>  
+      </li>
+      <li>
+        <router-link to="/read/sql_CRUD">4-7. CRUD</router-link>  
+      </li>
+      <li>
+        <router-link to="/read/sql_normal">4-8. 常用函數</router-link>  
+      </li>
+      <li>
+        <router-link to="/read/sql_join">4-9. 多表連接</router-link>  
+      </li> -->
+
+      <li>
+        <router-link to="/read/sqlTransaction">交易</router-link>  
+      </li>
+      <li>
+        <router-link to="/read/sqlTransaction _simple">交易-easy</router-link>  
+      </li>
+
+
     </ul>
   </div>
 </template>
