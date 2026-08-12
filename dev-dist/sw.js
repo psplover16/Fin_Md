512aa024900f223cfec098f41b0819bbf7901ae6
gary_lin <gary_lin@cmoney.com.tw>
2026-08-12 17:06:02 +0800

    交易

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.4m3dual3cog"
+    "revision": "0.hfb00pl5kkg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
