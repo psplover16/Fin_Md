d92ac8909c832537379dace2689b9cb2674679d2
gary_lin <gary_lin@cmoney.com.tw>
2026-08-14 15:14:02 +0800

    調整部分亂碼

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.hfb00pl5kkg"
+    "revision": "0.6i2eu8u2b2k"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
