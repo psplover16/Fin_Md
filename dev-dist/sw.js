36f94cf6d9c8c30741587dd44da5cb37585b4038
gary_lin <gary_lin@cmoney.com.tw>
2026-09-06 23:32:07 +0800

    調整

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.6mog2jbbd64"
+    "revision": "0.0out8r6rv0o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
